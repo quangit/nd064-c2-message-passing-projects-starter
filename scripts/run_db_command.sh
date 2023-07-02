3497db097c6d46a27baa32fa175dd6bee446e809
quangit <quang11t1@outlook.com>
2023-07-02 14:52:07 +0700

    update

--- scripts/run_db_command.sh
+++ scripts/run_db_command.sh
@@ -1,4 +1,7 @@
+# Usage: pass in the DB container ID as the argument
+# Set database configurations
+export CT_DB_USERNAME=ct_admin
 export CT_DB_NAME=geoconnections
-cat ./db/2020-08-15_init-db.sql | kubectl exec -i $1 -- bash -c "psql -U ct_admin -d geoconnections"
-cat ./db/udaconnect_public_person.sql | kubectl exec -i postgres-5c4dbcb7f-rqsnv -- bash -c "psql -U ct_admin -d geoconnections"
-cat ./db/udaconnect_public_location.sql | kubectl exec -i postgres-5c4dbcb7f-rqsnv -- bash -c "psql -U ct_admin -d geoconnections"
+cat ./db/2020-08-15_init-db.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+cat ./db/udaconnect_public_person.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+cat ./db/udaconnect_public_location.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
